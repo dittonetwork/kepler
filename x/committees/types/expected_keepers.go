ccd1f7f85769a0a0d90450ac92c814cb15f163fb
0fgiven <182217403+0fgiven@users.noreply.github.com>
2024-12-27 10:52:27 +0000

    add staking keeper interaction WIP

--- x/committees/types/expected_keepers.go
+++ x/committees/types/expected_keepers.go
@@ -1,5 +1,17 @@
 package types
 
+import (
+	context "context"
+
+	stakingtypes "cosmossdk.io/x/staking/types"
+)
+
 type EpochsKeeper interface {
 	// TODO Add methods imported from epochs should be defined here
+}
+
+type StakingKeeper interface {
+	// Add methods imported from staking should be defined here
+	GetBondedValidators(ctx context.Context) (validators []stakingtypes.Validator, err error)
+	UpdateValidatorsStakes(ctx context.Context, validatorsStakes map[string]uint64)
 }
