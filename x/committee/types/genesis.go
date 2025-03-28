080b53bb839058b13ac188b3d14ffb48204679c8
Anton Shchurov <rene.epicure@gmail.com>
2025-03-28 18:02:33 +0700

    206: added import and export genesis for committee

--- x/committee/types/genesis.go
+++ x/committee/types/genesis.go
@@ -2,14 +2,12 @@ package types
 
 // this line is used by starport scaffolding # genesis/types/import
 
-// DefaultIndex is the default global index.
-const DefaultIndex uint64 = 1
-
 // DefaultGenesis returns the default genesis state.
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
 		// this line is used by starport scaffolding # genesis/types/default
-		Params: DefaultParams(),
+		Params:    DefaultParams(),
+		LastEpoch: 0,
 	}
 }
 
