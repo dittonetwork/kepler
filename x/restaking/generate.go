98d37ed50adb2d4bd4f370dd7e238b2201ef7372
Anton Shchurov <rene.epicure@gmail.com>
2025-04-08 16:47:47 +0700

    chore(review): sugar changes

--- x/restaking/generate.go
+++ x/restaking/generate.go
@@ -1,4 +1,4 @@
 package restaking
 
 //go:generate mockgen --source=types/expected_keepers.go  --destination=testutil/expected_keepers_mocks.go --package=testutil
-//go:generate mockgen --source=types/expected_repository.go  --destination=testutil/expected_repository_mocks.go --package=testutil
+//go:generate mockgen --source=types/repository.go  --destination=testutil/repository_mocks.go --package=testutil
